1c226f783009ae5ba45df38a6e35baeaf5690275
Mpho Marufu <marufuem@gmail.com>
2023-03-20 16:17:41 -0700

    Program to print alphabets in lower and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,20 @@
 int main(void)
 {
 	char ch = 'a';
+	char CH = 'A';
 
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
 	putchar('\n');
 
-	return (0);
 
+	return (0);
 }
