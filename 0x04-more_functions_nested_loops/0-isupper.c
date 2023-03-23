85a8a452d618c3704cf7ffcaccdfd1c804ab2340
Mpho Marufu <marufuem@gmail.com>
2023-03-23 15:12:04 -0700

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - uppercase
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (1);
+}
