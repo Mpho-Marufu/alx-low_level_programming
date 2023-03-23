a5b7042728c083b3ecc98dfd9b168e2df04ff91f
Mpho Marufu <marufuem@gmail.com>
2023-03-23 16:01:24 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ *
+ * Return: values
+ */
+
+void print_numbers(void)
+{
+	while (c < 10)
+		_putchar(c);
+		c++;
+	putchar(c);
+}
+
