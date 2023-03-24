0a795d5e949ba944498900c2aeda128ad365345e
Mpho Marufu <marufuem@gmail.com>
2023-03-24 15:54:08 -0700

    print numbers besides 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ *
+ * Return: values
+ */
+
+void print_most_numbers(void)
+{
+        char c;
+
+        for (c = '0'; c <= '9'; c++)
+		if (c != '2' && c != '4')
+	                _putchar(c);
+
+        _putchar('\n');
+}
